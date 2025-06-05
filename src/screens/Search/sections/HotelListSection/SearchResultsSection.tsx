360f7d6863cf15890313d88b106027737babd4e8
Adrizzle113 <areahnaorea@gmail.com>
2025-06-05 07:33:39 -0700

    Browserless Error

--- src/screens/Search/sections/HotelListSection/SearchResultsSection.tsx
+++ src/screens/Search/sections/HotelListSection/SearchResultsSection.tsx
@@ -1,6 +1,6 @@
 import { Card, CardContent } from "../../../../components/ui/card";
 
-export const HotelListSection = (): JSX.Element => {
+export const SearchResultsSection = (): JSX.Element => {
   // Data for hotel images
   const hotelImages = [
     {
