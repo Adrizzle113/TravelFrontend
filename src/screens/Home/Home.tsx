d44cd3115c976ce97df4be0d00e390877222a6cb
Adrizzle113 <32805246+Adrizzle113@users.noreply.github.com>
2025-12-02 14:56:11 -0300

    Updated Home.tsx

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -4,6 +4,12 @@ export const Home = (): JSX.Element => {
   return (
     <main className="flex flex-col w-full overflow-x-hidden">
       <Navigation />
+      <section
+        className="relative w-full h-screen bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: "url('/images/HeroHotel.jpg')" }}
+      >
+        <div className="absolute inset-0 bg-black/30" />
+      </section>
     </main>
   );
 };
