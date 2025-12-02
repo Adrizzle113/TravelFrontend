1eddfcd58072ec9d5c4bd4aee12e6743d8db83ff
Adrizzle113 <32805246+Adrizzle113@users.noreply.github.com>
2025-12-02 14:12:16 -0300

    Updated Home.tsx

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,30 +1,9 @@
-
 import { Navigation } from "../../components/layout/Navigation";
-import { Header } from "../../components/layout/Header";
-import { StatisticsSection } from "./sections/StatisticsSection/StatisticsSection";
-import { AboutUsSection } from "./sections/AboutUsSection/AboutUsSection";
-import { IntroductionSection } from "./sections/IntroductionSection";
-import { ServicesSection } from "./sections/ServicesSection";
-import { BenefitsSection } from "./sections/BenefitsSection/BenefitsSection";
-import { PortfolioSection } from "./sections/PortfolioSection/PortfolioSection";
-import { ReviewsSection } from "./sections/ReviewsSection/ReviewsSection";
-import { ActionSection } from "./sections/ActionSection";
-import { ArticlesSection } from "./sections/ArticlesSection/ArticlesSection";
 
 export const Home = (): JSX.Element => {
   return (
     <main className="flex flex-col w-full overflow-x-hidden">
       <Navigation />
-      <Header />
-      <StatisticsSection />
-      <AboutUsSection />
-      <IntroductionSection />
-      <ServicesSection />
-      <BenefitsSection />
-      <PortfolioSection />
-      <ReviewsSection />
-      <ActionSection />
-      <ArticlesSection />
     </main>
   );
 };
