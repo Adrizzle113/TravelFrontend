a079a617b288a91a8fff0fb652902a81debc1b85
Adrizzle113 <32805246+Adrizzle113@users.noreply.github.com>
2025-12-06 10:50:14 -0300

    Updated NewHeroSection.tsx

--- src/screens/Home/sections/NewHeroSection/NewHeroSection.tsx
+++ src/screens/Home/sections/NewHeroSection/NewHeroSection.tsx
@@ -72,7 +72,7 @@ export const NewHeroSection = (): JSX.Element => {
                   <img
                     src="https://images.pexels.com/photos/1020974/pexels-photo-1020974.jpeg?auto=compress&cs=tinysrgb&w=800"
                     alt="European Street"
-                    className="w-full h-[300px] object-cover"
+                    className="w-full h-[250px] object-cover"
                   />
                 </div>
               </div>
