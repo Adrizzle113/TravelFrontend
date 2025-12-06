79a6b1383e5428d5d66b506a11863c70aecd8fbf
Adrizzle113 <32805246+Adrizzle113@users.noreply.github.com>
2025-12-06 10:20:07 -0300

    Updated NewHeroSection.tsx

--- src/screens/Home/sections/NewHeroSection/NewHeroSection.tsx
+++ src/screens/Home/sections/NewHeroSection/NewHeroSection.tsx
@@ -67,7 +67,7 @@ export const NewHeroSection = (): JSX.Element => {
               </h1>
 
               {/* Large rounded image */}
-              <div className="relative mt-8">
+              <div className="relative mt-20">
                 <div className="w-full max-w-md rounded-full overflow-hidden shadow-xl">
                   <img
                     src="https://images.pexels.com/photos/1020974/pexels-photo-1020974.jpeg?auto=compress&cs=tinysrgb&w=800"
